a6a88404a3b36cc68fff6a5e73b0fbd8ff09ae51
manisekharan <manisekhar2000@yahoo.com>
2020-05-28 08:46:10 -0400

    Add logs

--- src/main/java/org/tamil/timeline/webapp/EventController.java
+++ src/main/java/org/tamil/timeline/webapp/EventController.java
@@ -1,6 +1,8 @@
 package org.tamil.timeline.webapp;
 
 import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,12 +12,14 @@ import org.tamil.timeline.service.EventService;
 
 @RestController
 public class EventController {
+    private static final Logger logger = LoggerFactory.getLogger(EventController.class);
 
     @Autowired
     private EventService eventService;
 
     @GetMapping("/events")
     public List getAllEvents() {
+        logger.info("\n----Calling EventController.getAllEvents()----");
         return eventService.getAllEvents();
     }
 
